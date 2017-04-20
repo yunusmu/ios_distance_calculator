2668d5721033cc9766690310e1de59a0a9b77eab
Joseph Apps <joseph@Josephs-Mac.local>
2017-04-20 22:20:29 +0300

    Initial Commit

---
+++ DistanceCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DistanceCalculator
+//
+//  Created by Joseph Apps on 20/04/2017.
+//  Copyright © 2017 Joseph Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
